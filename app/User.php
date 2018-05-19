cff61576e992eb5205a8c2c7ebb679e44c45a869
plyschik <palyschik@gmail.com>
2018-05-19 22:02:25 +0200

    Soft delete to user model added.

--- app/User.php
+++ app/User.php
@@ -4,11 +4,12 @@ namespace App;
 
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable, HasRoles;
+    use Notifiable, SoftDeletes, HasRoles;
 
     /**
      * The attributes that are mass assignable.
